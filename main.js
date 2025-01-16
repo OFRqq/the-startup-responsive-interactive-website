3a5832072c86a566501fa7bf6bb1e6bea5dd3b1d
OFRqq <ole.franssen@gmail.com>
2025-01-16 18:08:44 +0100

    JS HTML FINISHING TOUCH #7 #4

---
+++ main.js
@@ -0,0 +1,13 @@
+// selecteerd de bow tie knop met interactie naam toggleSearch
+let toggleSearch = document.querySelector('.bow-tie')
+
+// selecteerd de searchbar-fold div met interactie naam toggleSearch
+let targettedDiv = document.querySelector('.searchbar-fold');
+
+// eventlistener voor toggleSearch via click, function naam ''fold''
+toggleSearch.addEventListener('click', fold)
+
+// function fold toggle classlist ''Jump''
+function fold(e) {
+    targettedDiv.classList.toggle('jump')
+}
