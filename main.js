a946e23aac18a7d8e3052bef4f6e326ea04dea7e
OFRqq <ole.franssen@gmail.com>
2025-01-21 15:48:00 +0100

    CSS HTML #6 #19 #5
    + class changes
    +article section changes
    
    + added quarries
    + more css
    
    +* {margin: 0;
        padding: 0;}
    + ul, a, a{
            list-style-type: none;
            text-decoration: none;
            color:var(--text-color-black)
        }

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // selecteerd de bow tie knop met interactie naam toggleSearch
 let toggleSearch = document.querySelector('.bow-tie')
 // selecteerd de searchbar-fold div met interactie naam targettedDiv
-let targettedDiv = document.querySelector('.searchbar-fold');
+let targettedDiv = document.querySelector('.searchbar');
 
 // eventlistener voor toggleSearch via click, function naam ''fold''
 toggleSearch.addEventListener('click', fold)
