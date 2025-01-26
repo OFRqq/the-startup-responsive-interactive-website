24b88339f78d5f2d03453a4129ce204c5faa8947
OFRqq <ole.franssen@gmail.com>
2025-01-26 13:47:09 +0100

    Update main.js small comment fix #18

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let targettedDiv = document.querySelector('.searchbar');
 // eventlistener voor toggleSearch via click, function naam ''fold''
 toggleSearch.addEventListener('click', fold)
 
-// function fold toggle classlist ''Jump''
+// function fold toggle classlist ''show''
 function fold(e) {
     targettedDiv.classList.toggle('show')
 }
